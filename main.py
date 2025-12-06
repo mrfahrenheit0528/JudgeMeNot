7816605df8f37b60e18497e8debcbedc6b601709
mrfahrenheit0528 <guhermoso@my.cspc.edu.ph>
2025-12-06 12:59:17 +0800

    turned into web app for easy testing

--- main.py
+++ main.py
@@ -126,4 +126,4 @@ def main(page: ft.Page):
     page.go(page.route)
 
 if __name__ == "__main__":
-    ft.app(target=main)
+    ft.app(target=main, view=ft.AppView.WEB_BROWSER, port=8550, host="0.0.0.0")
